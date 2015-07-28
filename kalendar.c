852755f07defa80ae9bfd5ac77b20f02c01e812a
wykys <wyk.reg@email.cz>
2015-07-28 02:56:13 +0200

    Update kalendar.c

--- kalendar.c
+++ kalendar.c
@@ -2,15 +2,15 @@
  * Kalendář pro Adama
  * wykys 2015
  * verze: 1.0
- * program vypiše kalenddář ve dvouch sloupcích pro jeden rok
+ * program vypíše kalendář ve dvouch sloupcích pro jeden rok
  * ovladani:
  * po zapnutí je uživatel vyzvám k tomu, aby zadal, kterým dnem začíná rok,
- * uživatel rok vyere zadáním čísla z intervalu <1;7> , kde 1 odpovídá Pondělí
+ * uživatel rok vyere zadáním čísla z intervalu <1;7> , kde 1 odpovídá pondělí
  * a 7 odpovídá pátku. Dále je uživatel vyzván k tomu, aby zadal, zda-li je vypisovaný
  * rok přestupný či nikoliv.
- * Program se ukončí ztiskem libovolné klávesy po vypsání kalendáře.
+ * Program se ukončí stiskem libovolné klávesy po vypsání kalendáře.
  *
- * Tento program je určer, jako ukázka řešenídomárího úkolu na Cčkovská wykysovská sezení :)
+ * Tento program je určer, jako ukázka řešení domácího úkolu na Cčkovská wykysovská sezení :)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
